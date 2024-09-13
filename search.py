978e43b7de7ff51eeb60c3f9728df07ad98c2703
R CHETAN KUMAR <68585775+SAVEGA07@users.noreply.github.com>
2024-09-12 21:24:44 -0400

    search function implement ENPM611

--- search.py
+++ search.py
@@ -5,4 +5,12 @@ def search_json(json_data, search_string):
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
+
+    # Searchs for the value 
+    for item in json_data:
+        for values in item:
+            # print(item[values])
+            if item[values] == search_string:
+                results.append(item)
+        
     return results
